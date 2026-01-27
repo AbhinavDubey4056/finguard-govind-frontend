9718fc25902bbf2b1d809f8b16ea31f92cfc9b28
Abhinav Dubey <128042307+AbhinavDubey4056@users.noreply.github.com>
2026-01-27 22:00:01 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -900,7 +900,7 @@ def login_page():
                 <p class="sidebar-text">
                     1. Authenticate your username.<br>
                     2. Input the password.<br>
-                    3. Complete the 5-phrase sequence verification.<br>
+                    3. Complete the 5-phrase sequence verification i.e finguard, ai, is, the, best .<br>
                     4. System access is logged and monitored.
                 </p>
                 <h3>About</h3>
